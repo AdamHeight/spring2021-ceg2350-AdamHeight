be7eb226fc484c243c9ba6e2a1ca2e17302f6a95
Adam Height <height.4@wright.edu>
2021-02-20 04:55:55 +0000

    Lab05 w/XC

--- Lab05/Echo.java
+++ Lab05/Echo.java
@@ -3,11 +3,14 @@ import java.util.*;
 class Echo {
 	  public static void main(String[] args) {
 		      Scanner sc = new Scanner(System.in);
+		      
+			System.out.println("What is your name?");
+			String name = sc.next();
+			System.out.println("Hello, " + name + ".");
+			
+			sc.close();
 
-		          System.out.println("What is your name?");
-			      String name = sc.next();
-			          System.out.println("Hello, " + name + ".");
-
-				      sc.close();
-				        }
+			MyFrame f = new MyFrame();
+			f.setVisible(true);
+	 }
 }
